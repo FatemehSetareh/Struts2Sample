b35c8b9985f4d1d730f7b0aa59b01b42948a06ce
DOTIN SCHOOL 4 <mjalalynia@gmail.com>
2015-12-08 14:38:31 +0330

    ok

---
+++ src/action/Register.java
@@ -0,0 +1,35 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Register extends ActionSupport {
+    private String name,password,email;
+
+    public String execute(){
+        return "success";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
